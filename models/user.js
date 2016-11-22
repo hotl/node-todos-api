2ba6e9be4c9099659a220366d3139e8e0935a499
Timothy Ho <timothyho0820@gmail.com>
2016-11-21 17:19:20 -0800

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('user', {
 		email: {
@@ -8,21 +11,42 @@ module.exports = function(sequelize, DataTypes) {
 				isEmail: true
 			}
 		},
+		salt: { // appends random characters to end of plain-text password
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
 		password: {
-			type: DataTypes.STRING,
+			type: DataTypes.VIRTUAL, // Do not persist this attribute to the database
 			allowNull: false,
 			validate: {
 				len: [8, 100],
 				is: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i
+			},
+			set: function(password) {
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(password, salt);
+
+				this.setDataValue('password', password);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
 			}
 		}
 	}, {
 		hooks: {
 			beforeValidate: function(user) {
 				if (typeof user.email === 'string') {
-					user.email = user.email.toLowerCase();	
+					user.email = user.email.toLowerCase();
 				}
 			}
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			}
 		}
+
 	})
 }
--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user) {
-		return res.json(user.toJSON());
+		return res.json(user.toPublicJSON());
 	}, function(err) {
 		res.status(400).json(err);
 	});
