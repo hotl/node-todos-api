684da318f9452fce584df16b576c7b2c2385f47a
Timothy Ho <timothyho0820@gmail.com>
2016-11-23 00:25:00 -0800

    Add token destroy: logout functionality

--- server.js
+++ server.js
@@ -143,17 +143,29 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
-		if (!token) {
-			return res.status(401).send();
-		}
-		res.header('Auth', token).json(user.toPublicJSON());
-	}, function(err) {
+		userInstance = user;
+
+		return db.token.create({
+			token: token
+		});
+	}).then(function(tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function(err) {
 		res.status(401).send();
-	})
-	
+	});
+});
+
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function(req, res) {
+	req.token.destroy().then(function() {
+		res.status(204).send();
+	}, function() {
+		res.status(500).send();
+	});
 });
 
 db.sequelize.sync({force: true}).then(function() {
